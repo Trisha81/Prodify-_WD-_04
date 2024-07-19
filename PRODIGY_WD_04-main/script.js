92cf578e2ec2ca78f02ea5bd152d066b747093e4
Trisha81 <trishaj961@gmail.com>
2024-07-19 22:40:35 +0530

    Add files via upload

---
+++ PRODIGY_WD_04-main/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const observerOptions = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    document.querySelectorAll('.animate').forEach(elm => {
+        observer.observe(elm);
+    });
+});
